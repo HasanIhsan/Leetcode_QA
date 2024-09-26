1fb0ca4faaeacd7d70094c4e6efca12a2e610083
Hassan Ihsan <ihsanhassan.business@gmail.com>
2024-09-25 18:07:21 -0700

    update: added mergerd sorted Array question

---
+++ 88 - merge sorted Array.py
@@ -0,0 +1,35 @@
+#! you are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+#! Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+#!The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+#! To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+#! and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
+
+#!Example 1:
+
+#* Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+#* Output: [1,2,2,3,5,6]
+#* Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+#* The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+def mergeArray(arr1, m, arr2, n):
+    #* merge the 2 arrays
+    for i in range(n):
+        arr1[m+i] = arr2[i]
+    
+    #* sort the merged array    
+    arr1.sort()
+    return arr1 #? return the mergerd array
+    
+    
+    
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 0, 0, 0 ]
+    m = 3
+    arr2 = [2, 5, 6]
+    n = 3
+    
+    print(mergeArray(arr1, m, arr2, n))   
