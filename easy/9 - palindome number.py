ca8044ea75fdf6e43a1d386ac475b0b6b17d0e86
Hassan Ihsan <ihsanhassan.business@gmail.com>
2024-09-27 15:37:15 -0700

    update: slight added a imeadly false for Q 9. cause of negatives

--- easy/9 - palindome number.py
+++ easy/9 - palindome number.py
@@ -10,6 +10,12 @@ def isPalindrome(num):
     ONum = num
     Rnum = 0
     
+    #* return false immeadly for negativs, cause a negaive number will never be a plindrome on the account of the face
+    #* that if the number is reverces aka say user enter -121 the output if accounting for plaindrome would be 121-
+    if num < 0:
+        return False
+        
+    #* reverse the number
     while num != 0:
         digit = num % 10
         Rnum = Rnum * 10 + digit
@@ -17,6 +23,10 @@ def isPalindrome(num):
     
     print("Original Number: ", ONum)
     print("Reverce Number: ", Rnum)
+    
+
+    
+    #* check if reversed number is the same as original number
     if ONum == Rnum:
         return True
     else:
